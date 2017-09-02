568650361350afef6bae3c25ca94922969c69090
showeimer <showeimer@gmail.com>
2017-09-02 17:04:14 -0400

    created handling for form,added state to CreatePost

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -4,19 +4,44 @@ class CreatePost extends Component {
   constructor(props) {
     super(props);
 
+    this.handleAuthorChange = this.handleAuthorChange.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleEntryChange = this.handleEntryChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      author: '',
+      title: '',
+      entry: ''
+    }
   }
 
+  handleAuthorChange = (event) => {
+    this.setState({author: event.target.value});
+  };
+
+  handleTitleChange = (event) => {
+    this.setState({title: event.target.value});
+  };
+
+  handleEntryChange = (event) => {
+    this.setState({entry: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
   render() {
     return(
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label htmlFor="author">Author:</label>
-        <input name="author" />
+        <input name="author" type="text" value={this.state.author}onChange={this.handleAuthorChange} />
 
         <label htmlFor="title">Title:</label>
-        <input name="title" />
+        <input name="title" type="text" value={this.state.title}onChange={this.handleTitleChange} />
 
         <label htmlFor="entry">Write your blog:</label>
-        <textarea name="entry" />
+        <textarea name="entry" type="text" value={this.state.entry}onChange={this.handleEntryChange} />
 
         <button>Submit</button>
       </form>
