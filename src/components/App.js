4e765da1bd9fdd3f8a870b811d0b7ebb82769a58
showeimer <showeimer@gmail.com>
2017-09-02 17:08:42 -0400

    added simple banner content to home page

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <h1>Welcome to BlogIt!</h1>
+        <h2>You blog it, we share it</h2>
       </div>
     );
   }
