34b5def20ee6f8445dc6d970fbe6b3f473ef990b
showeimer <showeimer@gmail.com>
2017-09-02 17:12:07 -0400

    basic component set up for PostList and ShowPost components

---
+++ src/components/PostList.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+import ShowPost from './ShowPost';
+
+class PostList extends Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default PostList;
---
+++ src/components/ShowPost.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ShowPost extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default ShowPost;
